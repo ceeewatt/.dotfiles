15d6485ecd7580a3cbcec6c62e11dbdafaf2426a
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2025-01-12 17:56:24 -0600

    update sandbox-setup.sh to initialize config.mk

--- scripts/sandbox-setup.sh
+++ scripts/sandbox-setup.sh
@@ -38,16 +38,32 @@ fi
 printf "Creating file '${SOURCE_FILE}'\n"
 touch ${SANDBOX_DIR}/${SOURCE_FILE}
 printf "\n\nint main(int argc, char* argv[])\n{" >> ${SANDBOX_DIR}/${SOURCE_FILE}
+printf "\n  (void)argc; (void)argv;" >> ${SANDBOX_DIR}/${SOURCE_FILE}
 printf "\n\n  return 0;\n}\n" >> ${SANDBOX_DIR}/${SOURCE_FILE}
 
 # Attempt to copy over an existing Makefile
 MAKEFILE_ONE="${MAKEFILE_DIR}/Makefile"
-MAKEFILE_TWO="${MAKEFILE_DIR}/config.mk"
+MAKEFILE_TWO="${MAKEFILE_DIR}/config-template.mk"
 if [ -f ${MAKEFILE_ONE} ] && [ -f ${MAKEFILE_TWO} ]
 then
   printf "Copying '${MAKEFILE_ONE}' and '${MAKEFILE_TWO}'"
   printf " into '${SANDBOX_DIR}'\n"
-  cp ${MAKEFILE_ONE} ${MAKEFILE_TWO} ${SANDBOX_DIR}
+  cp ${MAKEFILE_ONE} ${SANDBOX_DIR}
+  cp ${MAKEFILE_TWO} ${SANDBOX_DIR}/config.mk
+
+  # The file extension is expected to be either '.c' or '.cpp'.
+  # Use the file extension to set the LANGUAGE setting in the
+  #  sed command to the appropriate value.
+  EXTENSION="${SOURCE_FILE##*.}"
+  if [ ${EXTENSION} = c ] || [ ${EXTENSION} = C ]; then LANG='C'; else LANG='CPP'; fi
+
+  # Edit the config file to match our source file. This will break
+  #  if we can't match the strings 'LANGUAGE := C' and 'SRCS :='
+  #  exactly in the config file.
+  sed -i \
+    -e "0,/LANGUAGE := C/{s/LANGUAGE := C/LANGUAGE := ${LANG}/}" \
+    -e "0,/SRCS :=/{s/SRCS :=/SRCS := ${SOURCE_FILE}/}" \
+    ${SANDBOX_DIR}/config.mk
 else
   printf "Skipping Makefile... either '${MAKEFILE_ONE}' or"
   printf " '${MAKEFILE_TWO}' could not be found\n"
