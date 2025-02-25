c4132e65687ce5ce322a84da0c8279d7ae09fc4f
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2025-02-24 18:15:56 -0600

    nvim: mini.nvim plugins - mini.surround: easily add/delete surrounding paranthesis, brackets,   etc (#12) - mini.comment: easily toggle comments (#11) - mini.pairs: autopairs for paranthesis, quotes, etc

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,24 @@
+-- mini.nvim is a collection of independent plugins. Each plugin
+--  can be enabled separately through calling its respective
+--  setup function.
+
+local spec = {
+  'echasnovski/mini.nvim',
+
+  -- Stable branch
+  version = '*',
+
+  -- To enable a given module, call its setup function here
+  config = function()
+    -- Surround actions (add `sa`, delete `sd`, replace `sr`, etc)
+    require('mini.surround').setup()
+
+    -- Autopairs for quotes, paranthesis, brackets, etc
+    require('mini.pairs').setup()
+
+    -- Toggle comments
+    require('mini.comment').setup()
+  end
+}
+
+return spec
