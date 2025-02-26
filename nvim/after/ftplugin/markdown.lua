c4f29e048f484b05d0c5f72fc00f7d3167a5af7a
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2025-02-25 20:54:03 -0600

    nvim: make markdown.lua keymaps local to buffer

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.cmd("setlocal linebreak")
 -- vim.cmd("setlocal spell")
 
 -- For navigating visual lines: https://neovim.io/doc/user/usr_25.html#_breaking-at-words
-vim.keymap.set({ "n", "o", "x" }, "j", "gj", {})
-vim.keymap.set({ "n", "o", "x" }, "k", "gk", {})
-vim.keymap.set({ "n", "o", "x" }, "0", "g0", {})
-vim.keymap.set({ "n", "o", "x" }, "$", "g$", {})
+vim.keymap.set({ "n", "o", "x" }, "j", "gj", { buffer = 0 })
+vim.keymap.set({ "n", "o", "x" }, "k", "gk", { buffer = 0 })
+vim.keymap.set({ "n", "o", "x" }, "0", "g0", { buffer = 0 })
+vim.keymap.set({ "n", "o", "x" }, "$", "g$", { buffer = 0 })
