3f7f5bc3f9d3206ac105be421ca28a20e7351a2e
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2025-04-15 20:13:00 -0500

    nvim: easier window movements using leader key

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,3 +5,13 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Leave terminal mode" })
+
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus window right" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Focus window up" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Focus window down" })
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Focus window left" })
+
+vim.keymap.set("n", "<leader>L", "<C-w>L", { desc = "Move window right" })
+vim.keymap.set("n", "<leader>K", "<C-w>K", { desc = "Move window up" })
+vim.keymap.set("n", "<leader>J", "<C-w>J", { desc = "Move window down" })
+vim.keymap.set("n", "<leader>H", "<C-w>H", { desc = "Move window left" })
